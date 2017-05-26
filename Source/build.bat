d9281c13655eedc22afdc9a243a186cc29f18281
Sebastián Nale <sebastian.nale@nektra.com>
2017-05-26 14:13:55 -0300

    Updated solution and build script to use VS 2017.

--- Source/build.bat
+++ Source/build.bat
@@ -5,21 +5,16 @@ SET __Config=Release
 IF /I [%1] == [debug] (
     SET __Config=Debug
 )
-IF "%VS140COMNTOOLS%" == "" (
-    ECHO Visual Studio 2015 not found on this computer
+IF NOT EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" (
+    ECHO Visual Studio 2017 not found on this computer
     PAUSE
     GOTO end
 )
 
 :do_process
 SETLOCAL
-CALL "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
-IF "%VS140COMNTOOLS%" == "" (
-    ENDLOCAL
-    ECHO Cannot initialize Visual Studio x86 Command Prompt environment
-    PAUSE
-    GOTO end
-)
+CALL "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
+
 DEVENV DvEngine.sln /rebuild "%__Config%|Win32"
 IF NOT %ERRORLEVEL% == 0 (
     ENDLOCAL
@@ -27,16 +22,7 @@ IF NOT %ERRORLEVEL% == 0 (
     PAUSE
     GOTO end
 )
-ENDLOCAL
 
-SETLOCAL
-CALL "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x64
-IF "%VS140COMNTOOLS%" == "" (
-    ENDLOCAL
-    ECHO Cannot initialize Visual Studio x64 Command Prompt environment
-    PAUSE
-    GOTO end
-)
 DEVENV DvEngine.sln /rebuild "%__Config%|x64"
 IF NOT %ERRORLEVEL% == 0 (
     ENDLOCAL
